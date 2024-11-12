64f0579d2e55047a407998218e7f9ea7c5ee446c
fernandocruz184 <fernandocruz184@gmail.com>
2024-11-12 23:25:52 +0000

    bump to v0.1.5

--- etl_processor/__init__.py
+++ etl_processor/__init__.py
@@ -2,7 +2,7 @@ from .extract import FIRDSExtractor
 from .load import FIRDSLoader
 from .transform import FIRDSTransformer
 
-__version__ = '0.1.3'
+__version__ = '0.1.5'
 
 __all__ = [
     'FIRDSExtractor',
